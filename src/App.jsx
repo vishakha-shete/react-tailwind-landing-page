4d8292fa95d0d5aa4df3e8a5214ecd64a0dbf141
vishakha-shete <vishakhashete2@gmail.com>
2025-12-27 18:31:49 +0530

    Add new stats section with responsive layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from './Components/Navbar.jsx'
 import Hero from './Components/Hero.jsx'
 import Box from './Components/Box.jsx'
+import Section from './Components/Section.jsx'
 const App = () => {
   return (
     <div className=" min-h-screen w-full bg-black ">
       <Navbar />
       <Hero />
       <Box />
+      <Section />
     </div>
   )
 }
---
+++ src/Components/Section.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Section = () => {
+  return (
+    <div
+      className="
+        flex justify-center gap-60 text-white text-2xl
+        max-lg:gap-24
+        max-md:flex-wrap max-md:gap-12
+        max-sm:flex-col max-sm:items-center max-sm:gap-8
+      "
+    >
+      <div className="text-center">
+        <p className="text-1xl text-gray-500 ">DESIGNERS</p>
+        <h1 className=" text-center  text-5xl max-sm:ml-0">150+</h1>
+      </div>
+
+      <div className="text-center">
+        <p className="text-1xl text-gray-500 ">CLIENTS</p>
+        <h1 className=" text-center text-5xl max-sm:ml-0">500+</h1>
+      </div>
+
+      <div className="text-center">
+        <p className="text-1xl text-gray-500 ">MASTERPIECES</p>
+        <h1 className=" text-center text-5xl max-sm:ml-0">20K+</h1>
+      </div>
+
+      <div className="text-center">
+        <p className="text-1xl text-gray-500 ">EVENTS</p>
+        <h1 className=" text-center text-5xl  max-sm:ml-0">50+</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Section;
